bfc35f2bcc937e7ab38000eef85cf147342c4732
Victor Faria <victor.faria@luz.intra>
2020-11-06 16:21:54 -0300

    If an item is selected by code, show this item.
    If an item is selected by code, show this item.

--- MultiSelectTreeView/Controls/MultiSelectTreeViewItem.cs
+++ MultiSelectTreeView/Controls/MultiSelectTreeViewItem.cs
@@ -410,11 +410,12 @@ namespace System.Windows.Controls
 			{
 				if ((bool) e.NewValue)
 				{
-					if (!item.ParentTreeView.SelectedItems.Contains(item.DataContext))
+                    if (!item.ParentTreeView.SelectedItems.Contains(item.DataContext))
 					{
 						item.ParentTreeView.SelectedItems.Add(item.DataContext);
 					}
-				}
+                    item.BringIntoView();
+                }
 				else
 				{
 					item.ParentTreeView.SelectedItems.Remove(item.DataContext);
